2ea84e28e375eae4157c2e440592b4722745d8f8
Philipp Rembold <philipp@macuja.com>
2019-03-01 08:50:30 +0100

    Adds a framework around the lib

---
+++ Framework/SDAVAssetExportSession.h
@@ -0,0 +1,18 @@
+//
+//  SDAVAssetExportSession.h
+//  SDAVAssetExportSession
+//
+//  Created by Philipp Rembold on 01.03.19.
+//  Copyright © 2019 Boards & More. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SDAVAssetExportSession.
+FOUNDATION_EXPORT double SDAVAssetExportSessionVersionNumber;
+
+//! Project version string for SDAVAssetExportSession.
+FOUNDATION_EXPORT const unsigned char SDAVAssetExportSessionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDAVAssetExportSession/PublicHeader.h>
+#import "SDAVAssetExportSession/SDAVAssetExportSession.h"
