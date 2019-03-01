0520ca7a98f01b365c10623343cc8b57d6e56349
Philipp Rembold <philipp@macuja.com>
2019-03-01 08:54:32 +0100

    no message

--- Framework/SDAVAssetExportSessionFramework.h
+++ Framework/SDAVAssetExportSessionFramework.h
@@ -13,6 +13,3 @@ FOUNDATION_EXPORT double SDAVAssetExportSessionVersionNumber;
 
 //! Project version string for SDAVAssetExportSession.
 FOUNDATION_EXPORT const unsigned char SDAVAssetExportSessionVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <SDAVAssetExportSession/PublicHeader.h>
-#import "SDAVAssetExportSession/SDAVAssetExportSession.h"
